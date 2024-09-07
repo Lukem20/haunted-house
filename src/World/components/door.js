a5e381f1cb3b45234060d35d8cf154015cb5d04a
Luke <lukemoore237@gmail.com>
2024-09-06 19:31:46 -0700

    Updated file paths

--- src/World/components/door.js
+++ src/World/components/door.js
@@ -7,15 +7,14 @@ import {
 
 function createDoor() {
     function createMaterial () {
-        // Create and texture loader and load the texture
         const textureLoader = new TextureLoader();
-        const textureBaseColor = textureLoader.load('/assets/textures/woodendoor/color.jpg');
-        const textureNormalMap = textureLoader.load('/assets/textures/woodendoor/normal.jpg');
-        const textureHeightMap = textureLoader.load('/assets/textures/woodendoor/height.jpg');
-        const textureRoughessMap = textureLoader.load('/assets/textures/woodendoor/roughness.jpg');
-        const textureAmbientOcclusionMap = textureLoader.load('/assets/textures/woodendoor/ambientOcclusion.jpg');
-        const textureMetallic = textureLoader.load('/assets/textures/woodendoor/metalness.jpg');
-        const textureAlpha = textureLoader.load('/assets/textures/woodendoor/alpha.jpg')
+        const textureBaseColor = textureLoader.load('/assets/textures/door/color.jpg');
+        const textureNormalMap = textureLoader.load('/assets/textures/door/normal.jpg');
+        const textureHeightMap = textureLoader.load('/assets/textures/door/height.jpg');
+        const textureRoughessMap = textureLoader.load('/assets/textures/door/roughness.jpg');
+        const textureAmbientOcclusionMap = textureLoader.load('/assets/textures/door/ambientOcclusion.jpg');
+        const textureMetallic = textureLoader.load('/assets/textures/door/metalness.jpg');
+        const textureAlpha = textureLoader.load('/assets/textures/door/alpha.jpg')
 
         // Create a material using the loaded texture as a color map.
         const material = new MeshStandardMaterial({ 
